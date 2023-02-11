6a29fd6bda72717d5dbc5629cb87f3c2e822512f
José Ramón Ortiz Castañeda <joseramonortizcastaneda@MacBook-Pro-de-Jose-2.local>
2023-02-10 22:33:13 -0600

    Implementación uno del juego de la ruleta

--- Semanales/SemanalDos/SemanalDos.java
+++ Semanales/SemanalDos/SemanalDos.java
@@ -1,54 +1,37 @@
-import java.io.ObjectInputFilter.Status;
 import java.util.Arrays;
 
+public class hey{
 
-public class SemanalDos{
-
-    static int indiceArma = 0, largo = 0, porEliminar = 0, eliminados = 0,auxA = 1;
-    static int medición = 0;
-
-    static public void reto(int arr[],int inicio){
-        largo = arr.length-1; // 100
-        indiceArma = inicio; // 0 - 2 - - -
-    
-
-        porEliminar = indiceArma+auxA; // 1 - 3
-        indiceArma = porEliminar+auxA; // 2 - 
-    
-        if( porEliminar > largo ){
-            porEliminar = ((porEliminar)-largo)-1;
-            auxA++;
-
+    public static void juego(int[] jugadores, boolean ultimoMataPrimero){
+        int largo = jugadores.length;
+        if(jugadores.length == 1){
+            System.out.println("¡El jugador sobreviviene es: "+jugadores[0]+"!");
+            return;
+        }else{
+            if(largo%2==0){
+                int[] sobrevivientes = new int[(largo/2)];
+                int valorI = ultimoMataPrimero ? 1 : 0;
+                for(int i = 0, j = valorI; i < sobrevivientes.length && j < largo; i++, j=j+2)
+                    sobrevivientes[i] = jugadores[j];
+                System.out.println(Arrays.toString(sobrevivientes));
+                boolean ump = sobrevivientes[sobrevivientes.length-1]==jugadores[jugadores.length-1] ? true : false;
+                juego(sobrevivientes, ump);
+            }else{ 
+                int[] sobrevivientes = ultimoMataPrimero ?  new int[(largo/2)] : new int[(largo/2)+1];
+                int valorI = ultimoMataPrimero ? 1 : 0;
+                for(int i = 0, j = valorI; i < sobrevivientes.length && j < largo; i++, j=j+2)
+                    sobrevivientes[i] = jugadores[j];
+                System.out.println(Arrays.toString(sobrevivientes));
+                boolean ump = sobrevivientes[sobrevivientes.length-1]==jugadores[jugadores.length-1] ? true : false;
+                juego(sobrevivientes, ump);
+            }
         }
-
-        if( indiceArma > largo ){
-            indiceArma = ((indiceArma)-largo)-1;
-            auxA++;
-
-        }
-
-        //indiceArma = indiceArma >= largo ? (largo)-indiceArma : porEliminar;
-
-        arr[porEliminar] = 0;
-        eliminados++;
-
-        //System.out.println(Arrays.toString(arr)+" porEliminar: "+porEliminar+" pues >= "+largo+" da"+(largo-porEliminar));
-        System.out.println(" - Aquí está el arma: "+indiceArma+" y el eliminado fue: "+porEliminar+Arrays.toString(arr)+". AuxA:"+auxA+" y AuxE: "+auxA);
-
-
-        if(eliminados!=largo){
-            medición++;
-            reto(arr,indiceArma);
-        }
-
     }
-
     public static void main(String[] args) {
-        int[] arr = new int[5];
-        for (int i = 0; i < 5; i++) {
-            arr[i] = 1;
+        int[] arr = new int[100];
+        for (int i = 0; i < 100; i++) {
+            arr[i] = (i+1);
         }
-        reto(arr, 0);
-        System.out.println("¡Con un total de "+medición+" iteraciones!");
+        juego(arr, false);
     }
 }
