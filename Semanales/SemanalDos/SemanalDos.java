6d7ac5b726f6585204891b14eba3b429321071b9
José Ramón Ortiz Castañeda <joseramonortizcastaneda@MacBook-Pro-de-Jose-2.local>
2023-02-12 18:08:32 -0600

    Final Ruleta

--- Semanales/SemanalDos/SemanalDos.java
+++ Semanales/SemanalDos/SemanalDos.java
@@ -1,26 +1,30 @@
 import java.util.Arrays;
-
-public class hey{
-
+public class SemanalDos{
+    static int operaciones = 0;
     public static void juego(int[] jugadores, boolean ultimoMataPrimero){
         int largo = jugadores.length;
         if(jugadores.length == 1){
-            System.out.println("¡El jugador sobreviviene es: "+jugadores[0]+"!");
+            System.out.println("¡El jugador sobreviviente es: "+jugadores[0]+"!\nCon "+operaciones+" operaciones.");
+
             return;
         }else{
             if(largo%2==0){
                 int[] sobrevivientes = new int[(largo/2)];
-                int valorI = ultimoMataPrimero ? 1 : 0;
-                for(int i = 0, j = valorI; i < sobrevivientes.length && j < largo; i++, j=j+2)
+                int valorDeInicio = ultimoMataPrimero ? 1 : 0;
+                for(int i = 0, j = valorDeInicio; i < sobrevivientes.length && j < largo; i++, j=j+2){
                     sobrevivientes[i] = jugadores[j];
+                    operaciones++;
+                }
                 System.out.println(Arrays.toString(sobrevivientes));
                 boolean ump = sobrevivientes[sobrevivientes.length-1]==jugadores[jugadores.length-1] ? true : false;
                 juego(sobrevivientes, ump);
             }else{ 
                 int[] sobrevivientes = ultimoMataPrimero ?  new int[(largo/2)] : new int[(largo/2)+1];
-                int valorI = ultimoMataPrimero ? 1 : 0;
-                for(int i = 0, j = valorI; i < sobrevivientes.length && j < largo; i++, j=j+2)
+                int valorDeInicio = ultimoMataPrimero ? 1 : 0;
+                for(int i = 0, j = valorDeInicio; i < sobrevivientes.length && j < largo; i++, j=j+2){
                     sobrevivientes[i] = jugadores[j];
+                    operaciones++;
+                }
                 System.out.println(Arrays.toString(sobrevivientes));
                 boolean ump = sobrevivientes[sobrevivientes.length-1]==jugadores[jugadores.length-1] ? true : false;
                 juego(sobrevivientes, ump);
@@ -28,8 +32,9 @@ public class hey{
         }
     }
     public static void main(String[] args) {
-        int[] arr = new int[100];
-        for (int i = 0; i < 100; i++) {
+        int n = 100;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
             arr[i] = (i+1);
         }
         juego(arr, false);
